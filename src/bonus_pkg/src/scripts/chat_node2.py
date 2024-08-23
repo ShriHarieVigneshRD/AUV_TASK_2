de3a892e501fc4f4379e15ff0fc5e75a5f847456
ShriHarieVigneshRD <157583010+ShriHarieVigneshRD@users.noreply.github.com>
2024-08-23 13:10:24 +0530

    Update chat_node2.py

--- src/bonus_pkg/src/scripts/chat_node2.py
+++ src/bonus_pkg/src/scripts/chat_node2.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
+from bonus_pkg.msg import node1message  # Custom message for Node 1
+from bonus_pkg.msg import node2message   # Custom message for Node 2
 
-def callback(data):
-	#check if the message is not from this node
-	if data._connection_header['callerid']!=rospy.get_name():
-		rospy.loginfo("[%s] Received: %s",rospy.get_name(),data.data)
+def callback(msg):
+    rospy.loginfo(f"Node2 received: {msg.message1}")
 
-def node2():
-	rospy.init_node('chat_node2',anonymous=True)
+def node1():
+    rospy.init_node('node2', anonymous=True)
 
-	pub=rospy.Publisher('chat_bot',String,queue_size=10)
-	rospy.Subscriber('chat_bot',String,callback)
+    pub = rospy.Publisher('chat', node2message, queue_size=10)
+    rospy.Subscriber('chat', node1message, callback)
 
-	rate=rospy.Rate(10)
-	while not rospy.is_shutdown():
-		msg=input("[%s] Enter a message:" % rospy.get_name())
-		rospy.loginfo("[%s] Publishing: %s:",rospy.get_name(),msg)
-		pub.publish(msg)
-		rate.sleep()
+    rate = rospy.Rate(1)  # 1 Hz
+    while not rospy.is_shutdown():
+        msg = node2message()
+        msg.message2 = input("Node 2 getting input")
+        rospy.loginfo("Node2 sending: " + msg.message2)
+        pub.publish(msg)
+        rate.sleep()
 
-if __name__=="__main__":
-	try:
-		node2()
-	except rospy.ROSInterruptException:
-		pass
+if __name__ == '__main__':
+    try:
+        node1()
+    except rospy.ROSInterruptException:
+        pass
+
 		
